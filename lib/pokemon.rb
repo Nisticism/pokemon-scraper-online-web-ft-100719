062e22c417747ac1806aed9c91ec047efa744b06
Hans Foster <fosterhans@gmail.com>
2019-12-06 23:36:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,52 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  def initialize(id = nil, name = "", type = "", db)
+    @id = id
+    @name = name
+    @type = type 
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    if @id != nil
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type, db)
+        VALUES (?, ?, ?)
+      SQL
+ 
+      @db.execute(sql, name, type, db)
+      @id = @db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+  end
+  
+  def self.find(id)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    result = @db.execute(sql, id)[0]
+    Pokemon.new(result[0], result[1], result[2])
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    type = row[2]
+    db = row[3]
+    self.new(id, name, type, db)
+  end
+  
+  def update
+    sql = "UPDATE pokemon SET name = ?, type = ?, db = ?, WHERE id = ?"
+    @db.execute(sql, self.name, self.type, self.db, self.id)
+  end
+  
 end
